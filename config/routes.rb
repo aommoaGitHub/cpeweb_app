22d9bfef90226f882de104dca585f6b855415625
Pannapat Panpakdee <pannapat.pa@ku.th>
2017-05-25 08:06:52 +0000

    add activities pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
+  
+  get 'static_pages/activity'
+  
+  get 'static_pages/activityinside'
+  
+  get 'static_pages/meeting'
+  
+  get 'static_pages/charity'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
